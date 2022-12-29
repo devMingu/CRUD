6d028eb9571a8e6448cfffadaf408b88788995c7
devMingu <rooster100@naver.com>
2022-12-29 14:27:05 +0900

    feat: views 폴더를 연결, get 요청 보냄

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const engine = require('ejs-mate');
+const path = require('path');
 const PORT = 3000;
 
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', engine);
+
+app.get(`/`, (req, res) => {
+    res.render('home');
+})
 
 
 app.listen(PORT, ()=>{
